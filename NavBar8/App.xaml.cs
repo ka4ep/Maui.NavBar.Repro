ba999179e5cc8de22dd1fba936f865b6ebba8f7b
ka4ep <sergey@habital.lv>
2023-11-22 12:04:03 +0200

    Reproduction using .NET 7 and .NET 8

---
+++ NavBar8/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NavBar8
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
